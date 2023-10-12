fab324ed43762fdf27c2c310c4708471ca51b176
rnzim <rnxpphp@gamil.com>
2023-10-11 23:06:19 -0300

    fix

---
+++ index.js
@@ -0,0 +1,9 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://google.com');
+  await page.screenshot({path: 'example.png'});
+  await browser.close();
+})();
